18c732d117c3dea72c8d4bc80860cb0f01f41b5f
leighgawne <leighgawne@gmail.com>
2024-06-19 17:52:53 -0700

    Fix option

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = (env) => {
 		plugins,
 		devServer: {
 			open: false,
-			public: 'shsddemo.azurewebsites.net',
+			allowedHosts: [ 'shsddemo.azurewebsites.net' ],
 		},
 	};
 };
